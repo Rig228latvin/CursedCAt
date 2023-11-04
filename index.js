097aa5a904898e7e022712a7e4a24c0e6311e2de
Rig228latvin <123410462+Rig228latvin@users.noreply.github.com>
2023-11-04 22:03:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function helloWorld() {
+	console.log('Hello World!')
+	let d = 1000
+	div.innerText += '\n'
+	while (d >= 0) {
+		div.innerText += d + '\n'
+		d -= 7
+	}
+	return 0
+}
+const button = document.getElementById('button')
+const div = document.getElementById('div')
+button.addEventListener('click', helloWorld)
